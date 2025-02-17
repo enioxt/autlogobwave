122d1574e540cc165865b933c2dad9ee6055ccc5
enioxt <enioxt@gmail.com>
2025-02-17 14:53:41 -0300

    Update bot_logomarca.py

--- bot_logomarca.py
+++ bot_logomarca.py
@@ -36,4 +36,7 @@ def processar_logo(input_path, output_path):
     image.save(output_path)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
